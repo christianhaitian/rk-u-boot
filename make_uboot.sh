6a8e51c9f414756869c76c94280475d4baaca56b
christianhaitian <mckman@comcast.net>
2022-04-27 22:38:28 -0400

    Update make_uboot.sh

--- make_uboot.sh
+++ make_uboot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+./make.sh rk3566
+./make.sh trust
+./make.sh loader
+tools/mkimage -n rk356x -T rksd -d ../rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.08.bin:../rkbin/bin/rk35/rk356x_spl_v1.11.bin idblock.bin
 dd if=/dev/zero of=spi_image.img bs=1M count=0 seek=16
 /usr/sbin/parted -s spi_image.img mklabel gpt
 /usr/sbin/parted -s spi_image.img unit s mkpart idbloader 64 7167
